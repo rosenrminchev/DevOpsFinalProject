492bc877ddb01c4d80765c433b23ff7fa312099f
rosenrminchev <rminchev94@gmail.com>
2024-01-26 13:51:50 +0200

    typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,7 +141,7 @@ resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = data.aws_subnets.default.id
+  subnet_ids      = data.aws_subnets.default.ids
   scaling_config {
     desired_size = 1
     max_size     = 2
