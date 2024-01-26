1e2b0f106f082b7cd82888827e5ebbd5f54323d8
rosenrminchev <rminchev94@gmail.com>
2024-01-26 13:36:25 +0200

    Add EKS configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,3 +67,83 @@ resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_security_group" "selected" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
+
+
+# EKS Cluster Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }],
+    Version = "2012-10-17"
+  })
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = tolist(data.aws_subnet_ids.default.ids)
+  }
+}
+
+# EKS Node Group Role
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }],
+    Version = "2012-10-17"
+  })
+}
+
+
+
+# Attach required policies for EKS Node Group Role
+resource "aws_iam_role_policy_attachment" "eks_node_group_attach" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = tolist(data.aws_subnet_ids.default.ids)
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+}
+
+# VPC, Subnets, Security Groups
+# (You would already have this in your configuration, just ensure they are correctly referenced)
