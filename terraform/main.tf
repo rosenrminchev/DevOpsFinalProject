088754becca66a3d6b070245df14ff018dea9b1d
rosenrminchev <rminchev94@gmail.com>
2024-01-26 10:51:13 +0200

    Fix to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.0"
-    }
-  }
-  backend "s3" {
-    key = "aws/ec2-deploy/terraform.tfstate"
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-resource "aws_instance" "servernode" {
-  ami                    = "ami-052efd3df9dad4825"
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = var.private_key
-    timeout     = "4m"
-  }
-  tags = {
-    "name" = "DeployVM"
-  }
-}
 resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-profile"
   role = "ec2-ecr-authorization"
@@ -79,9 +47,4 @@ resource "aws_security_group" "maingroup" {
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
   public_key = var.public_key
-}
-
-output "instance_public_ip" {
-  value     = aws_instance.servernode.public_ip
-  sensitive = true
 }
